d3912c183d148a48acf067c7fd51afe1705e160d
Pavel Gun <pavel_gun@epam.com>
2021-04-27 20:16:15 +0300

    fix / dao, handler, mq, hibernate_conf, build.gradle

--- src/main/java/org/example/untitled/db/DocumentDao.java
+++ src/main/java/org/example/untitled/db/DocumentDao.java
@@ -5,7 +5,7 @@ import org.hibernate.Transaction;
 
 public class DocumentDao {
 
-    public Long save(DocumentEntity documentEntity) {
+    public static Long save(DocumentEntity documentEntity) {
         try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
             Transaction transaction = session.beginTransaction();
             final Long id = (Long) session.save(documentEntity);
--- src/test/java/HandlerTest.java
+++ src/test/java/HandlerTest.java
@@ -10,26 +10,22 @@ import javax.jms.JMSException;
 public class HandlerTest {
     /*private final DocumentDao documentDao = new DocumentDao();*/
 
-    private final ActiveMQConnectionFactory connectionFactory = Mq.createActiveMQConnectionFactory();
-
-    private final PooledConnectionFactory pooledConnectionFactory = Mq.createPooledConnectionFactory(connectionFactory);
 
     @Test
     void test() {
-        String input = "test";
+        /*String input = "test";
         String message = null;
         try {
-            Mq.sendMessage(pooledConnectionFactory, input);
-            message = Mq.receiveMessage(connectionFactory);
+            Mq.sendMessage(input);
+            message = Mq.receiveMessage();
         } catch (JMSException e) {
             e.printStackTrace();
         }
-        pooledConnectionFactory.stop();
 
         System.out.println(message);
 
-        /*final Long id = documentDao.save(new DocumentEntity(message));*/
+        final Long id = documentDao.save(new DocumentEntity(message));
 
-        /*System.out.println(id);*/
+        System.out.println(id);*/
     }
 }
